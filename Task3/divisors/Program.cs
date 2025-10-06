5f9bb07cd9f057bad9516454de98c38694825449
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-06 23:37:05 +0400

    divisors task added

---
+++ Task3/divisors/Program.cs
@@ -0,0 +1,6 @@
+﻿//Verilmiş n ədədinin bölənlərini tapın.
+System.Console.WriteLine("n - i daxil edin:");
+int n = int.Parse(System.Console.ReadLine());
+for (int i = 1; i <= n / 2; i++)
+    if (n % i == 0)
+        System.Console.WriteLine(i);
