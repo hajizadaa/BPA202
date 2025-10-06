d9c6f99af5776f8d9a4f5b42847c61dcb288bc65
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-06 23:59:02 +0400

    place value task added

---
+++ Task3/place_value/place_value/Program.cs
@@ -0,0 +1,24 @@
+﻿//Verilmiş n mərtəbəli ədədin ən yüksək mərtəbəsini tapın.
+System.Console.WriteLine("n - i daxil edin:");
+int n = int.Parse(System.Console.ReadLine());
+int mertebe = 0;
+int mertebeSayi = 0;
+while (n != 0)
+{
+    mertebe = n % 10;
+    n /= 10;
+    mertebeSayi++;
+}
+System.Console.WriteLine("n - in en yuksek mertebesi:");
+System.Console.WriteLine(mertebe);
+
+// elave
+System.Console.WriteLine("n nece mertebelidir:");
+System.Console.WriteLine(mertebeSayi);
+int power = mertebe;
+for (int i = 0; i < mertebeSayi - 1; i++)
+{
+    power *= 10;
+}
+System.Console.WriteLine("n en yuksek mertebe deyeri:");
+System.Console.WriteLine(power);
