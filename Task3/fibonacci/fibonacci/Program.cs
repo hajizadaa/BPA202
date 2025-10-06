9f8788b7ed56aca7fcb32b317d59e6b205e6e4d0
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-06 22:59:48 +0400

    fibonacci task added

---
+++ Task3/fibonacci/fibonacci/Program.cs
@@ -0,0 +1,14 @@
+﻿//Fibonacci ardıcıllığını ekrana çıxaran alqoritm yazın
+int fibo1 = 0, fibo2 = 1, fibo3 = 0;
+int elementNum;
+System.Console.WriteLine("Ardicilliqin Nece Elementi Yazdirilmalidir: ");
+elementNum = int.Parse(System.Console.ReadLine());
+Console.WriteLine(fibo1);
+Console.WriteLine(fibo2);
+for (int i = 2; i < elementNum; i++)
+{
+    fibo3 = fibo1 + fibo2;
+    System.Console.WriteLine(fibo3);
+    fibo1 = fibo2;
+    fibo2 = fibo3;
+}
