abd0ba70cd7e7c1ef9e19f7ae683edf32337c738
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-07 23:10:29 +0400

    programme 1 added

---
+++ task4/programme1/programme1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Verilmish ededin mertebelerinin en boyuyunu tapan alqoritm
+Console.WriteLine("Ededi Daxil Edin: ");
+int n = int.Parse(Console.ReadLine());
+int max = 0, r = 0;
+while (n != 0)
+{
+    r = n % 10;
+    if (r > max)
+        max = r;
+    n /= 10;
+}
+Console.WriteLine("En boyuk mertebe: " + max);
