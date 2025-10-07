71fbe9eb1553c3d37fa2d121b966b1e19e33715c
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-07 23:12:55 +0400

    programme 5 added

---
+++ task4/programme5/programme5/Program.cs
@@ -0,0 +1,18 @@
+﻿//Verilmish arrayda murekkeb ededlerin sayini cixaran algorithm
+int[] arr = new int[10];
+Console.WriteLine("Arrayi Daxil Edin: ");
+for (int i = 0; i < 10; i++)
+    arr[i] = int.Parse(Console.ReadLine());
+int bolenSay = 0, murekkebSay = 0;;
+for (int i = 0; i < 10; i++)
+{
+    bolenSay = 0;
+    for (int x = 1; x <= arr[i]; x++)
+    {
+        if (arr[i] % x == 0)
+            bolenSay++;
+    }
+    if (bolenSay > 2)
+        murekkebSay++;
+}
+Console.WriteLine("Murekkeb Eded Sayi: " + murekkebSay);
