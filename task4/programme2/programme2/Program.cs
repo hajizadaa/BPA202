2b86c63c46cd6f99bf7f1e27bb27836b825b51bb
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-07 23:11:18 +0400

    programme 2 added

---
+++ task4/programme2/programme2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Verilmish ededin 2 quvveti olub olmadigini tapan alqoritm
+Console.WriteLine("Ededi Daxil Edin:");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+while (n > i)
+    i *= 2;
+Console.WriteLine(i);
+if (n == i && i!= 1)
+    Console.WriteLine($"{n} 2 - nin quvvetidir.");
+else
+    Console.WriteLine($"{n} 2 - nin quvveti deyildir.");    
