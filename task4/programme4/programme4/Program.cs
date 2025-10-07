42564a4bc5c5d49262fd495df97a5338915f77d4
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-07 23:12:19 +0400

    programme 4 added

---
+++ task4/programme4/programme4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Verilmish arrayda hem tek eded olan hemde 3-e bolunen ededlerin sayini cixaran algorithm
+int[] arr = new int [10];
+Console.WriteLine("Arrayi Daxil Edin:");
+for (int i = 0; i < 10; i++)
+    arr[i] = int.Parse(Console.ReadLine());
+int say = 0;
+for (int i = 0; i < 10; i++)
+    if (arr[i] % 3 == 0 && arr[i] % 2 == 1)
+        say++;
+Console.WriteLine("Say: " + say);
