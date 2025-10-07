f0a86b4ed89eda2322de7f06518a999869aa62e4
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-07 23:11:48 +0400

    programme 3 added

---
+++ task4/programme3/programme3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Verilmish arrayda en kicik ve en boyuk ededlerin cemini cixaran alogirthm
+Console.WriteLine("Arrayi Daxil Edin:");
+int[] arr = new int[10];
+for (int i = 0; i < 10; i++)
+    arr[i] = int.Parse(Console.ReadLine());
+int min = arr[0], max = arr[0];
+for (int i = 0; i < 10; i++)
+{
+    if (arr[i] < min)
+        min = arr[i];
+    if (arr[i] > max)
+        max = arr[i];
+}
+int sum = min + max;
+Console.WriteLine("Sum: " + sum);
